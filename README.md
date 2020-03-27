e0346403baf90711f1f2bed01e1ccb54fbf305b0
Nicolas Gonzalez <ngonzalez@operametrix.fr>
2020-03-27 20:26:33 +0100

    Add licensing and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## OpxFlow : Open Source MQTT high performance Broker
+
+OpxFlow is an MQTT broker written in C optimize for high performance.
--- src/packets/decoder/packets_decoder.h
+++ src/packets/decoder/packets_decoder.h
@@ -1,3 +1,24 @@
+/**
+ * OpxFlow
+ * Copyright (c) 2020 OperaMetrix SAS
+ * Author: Nicolas Gonzalez <ngonzalez@operametrix.fr>
+ *
+ * This file is part of OpxFlow.
+ *
+ * OpxFlow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpxFlow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpxFlow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __PACKETS_DECODER_H__
 #define __PACKETS_DECODER_H__
 
